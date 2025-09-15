f276baa028cb077ace74a239d0b0cf6587182dca
Jester-Penetrate <fatavora@up.edu.ph>
2025-09-16 01:19:50 +0800

    improved player movement
    still need to improve camera movement

--- practice_shit/player.gd
+++ practice_shit/player.gd
@@ -1,25 +1,38 @@
 extends CharacterBody2D
 
 
-@export var speed = 300.0
-@export var JUMP_VELOCITY = -400.0
+@export var speed = 200.0
+@export var acceleration = 1000
+@export var friction = 1000
+@export var jump_velocity = -400.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	apply_gravity(delta)
+	jump()
+	var input_axis := Input.get_axis("ui_left", "ui_right")
+	handle_acceleration(input_axis, delta)
+	apply_friction(input_axis, delta)
+	move_and_slide()
+	
+func apply_gravity(delta):
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+func handle_acceleration(input_axis, delta):
+	if input_axis != 0:
+		velocity.x = move_toward(velocity.x, input_axis * speed, acceleration * delta)
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * speed
+func apply_friction(input_axis, delta):
+	if input_axis == 0:
+		velocity.x = move_toward(velocity.x, 0, friction * delta)
+
+func jump():
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_accept"):
+			velocity.y = jump_velocity
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
+		if Input.is_action_just_released("ui_accept") and velocity.y < jump_velocity/2:
+			velocity.y = jump_velocity/2
 
-	move_and_slide()
+	
